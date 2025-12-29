4c8823228aa6021ec72ad66f358b4c84a732d816
eashaop2023 <eashaop2023@gmail.com>
2025-12-29 10:38:35 +0530

    ceo image

--- src/components/Website/About-Us/Mission.jsx
+++ src/components/Website/About-Us/Mission.jsx
@@ -4,7 +4,8 @@ import Eashafounder from "../../../assets/easha-founder.jpg";
 // import Eashaceo from "../../../assets/easha-ceo.jpg"; 
 // import Eashaceo from "../../../assets/easha-ceo-final.jpg";
 // import Eashaceo from "../../../assets/Easha-ceo-final.jpg";
-import Eashaceo from "../../../assets/CEO-FINAL.jpg";
+// import Eashaceo from "../../../assets/CEO-FINAL.jpg";
+import Eashaceo from "../../../assets/ceo.jpg";
 const VisionMission = () => {
   return (
     <Container fluid className="py-5 px-4" style={{ backgroundColor: "#e6f2f3" }}>
@@ -62,7 +63,7 @@ const VisionMission = () => {
               boxShadow: "0 4px 12px rgba(0,0,0,0.1)",
             }}
           />
-          <h5 className="mt-3" style={{ color: "#0f1111ff" }}>Ms.Kasula Vaishnavi</h5>
+          <h5 className="mt-3" style={{ color: "#0f1111ff" }}>Ms. Nadimidoddi Salomi</h5>
           <h6 className="mt-1" style={{ color: "#0f1111ff" }}>Easha CEO</h6>
         </Col>
       </Row>
